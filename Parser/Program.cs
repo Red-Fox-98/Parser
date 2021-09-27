fdf833e92ffe5c40e0e4b403664df92bcfa20b84
Red-Fox-98 <evgeniya.zaitseva.98@gmail.com>
2021-09-27 19:27:01 +0600

    Added output resolution and image size

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -1,9 +1,13 @@
 ﻿using HtmlAgilityPack;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Net;
 using System.Text;
+using System.Threading;
 
 namespace Parser
 {
@@ -11,31 +15,78 @@ namespace Parser
     {
         static void Main(string[] args)
         {
+            //Enter link
+            Console.Write("Enter link: ");
+            string requestHtml = Console.ReadLine();
+            Console.WriteLine();
+            //Request HTML
             HtmlWeb wc = new HtmlWeb();
             wc.OverrideEncoding = Encoding.UTF8;
-            HtmlDocument docHtml = wc.Load("https://forum.awd.ru/viewtopic.php?f=1011&t=165935");
-
+            HtmlDocument docHtml = wc.Load(requestHtml);
+            //Array creation
             ArrayList list = new ArrayList();
-
-            int value = 0;
-
-            foreach (HtmlNode item in docHtml.DocumentNode.SelectNodes("//div[contains(@class, 'content')]//a/@href | //img/@data-src"))
+            //Index image
+            uint imageIndex = 0;
+            uint imgIndex = 0;
+            //Creating folder for image
+            Directory.CreateDirectory("Images");
+            //Search url image
+            foreach (HtmlNode item in docHtml.DocumentNode.SelectNodes("//img/@data-src"))
             {
-                Console.WriteLine(item.GetAttributeValue("data-src", null));
+                Console.WriteLine("URL image № " + imgIndex);
+                Console.Write(item.GetAttributeValue("data-src", null));
+                Console.Write("\n");
                 list.Add(item.GetAttributeValue("data-src", null));
+                imgIndex++;
             }
-
+            //Loading images
+            Console.Write("\n");
+            Console.WriteLine("Loading images...");
+            Console.Write("\n");
             foreach (string itemImg in list)
             {
                 if (itemImg != null)
                 {
                     using (WebClient client = new WebClient())
                     {
-                        client.DownloadFile(new Uri(itemImg), @"Images\image" + value++ + ".jpg");
+                        try
+                        {
+                            client.DownloadFile(new Uri(itemImg), @"Images\image" + imageIndex++ + ".jpg");
+                            Console.WriteLine("Downloding image: " + itemImg);
+                        }
+                        catch (Exception)
+                        {
+
+                            Console.WriteLine("Failed to download image\n");
+                            Console.Write(itemImg);
+                        }
                     }
                 }
-                else { }
+                else { continue; }
             }
+            Console.Write("\n");
+            Console.Write("Loading is complete\n");
+            Console.Write("\n");
+            //Index image
+            uint imageIndexInfo = 0;
+            Console.WriteLine("Information about uploaded images\n");
+            //Displaying image information
+            for (int i = 0; i < imageIndex; i++)
+            {
+                Image img = Image.FromFile(@"Images\image" + imageIndexInfo + ".jpg");
+                Console.WriteLine("Image №: " + imageIndexInfo);
+                Console.WriteLine("Image dimensions : " + img.Size);
+
+                double length = new FileInfo(@"Images\image" + imageIndexInfo + ".jpg").Length;
+                Console.Write(length /= 1024);
+                Console.Write(" Кб\n");
+                imageIndexInfo++;
+                img.Dispose();                
+            }
+            Console.WriteLine("Success, press enter");
+            Console.ReadLine();
+            //Deleting folder"
+            Directory.Delete("Images", true);
         }
     }
 }
